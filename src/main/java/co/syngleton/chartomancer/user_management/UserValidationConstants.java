3b7c5cb2a2fc598b8e98908bf2a286070f4fdcce
mactepluke <licensetospam@icloud.com>
2024-03-25 18:39:59 +0100

    Fixed the regex patterns and used email pattern for username

--- src/main/java/co/syngleton/chartomancer/user_management/UserValidationConstants.java
+++ src/main/java/co/syngleton/chartomancer/user_management/UserValidationConstants.java
@@ -6,10 +6,10 @@ public class UserValidationConstants {
         throw new IllegalStateException("Utility class");
     }
 
-    public static final String USERNAME_PATTERN = "[A-Za-zÀ-ÿ-]{3,50}$";
-    public static final String PASSWORD_PATTERN = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&]).{8,30}$";
-    public static final String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{6,50}$";
-    public static final String USERNAME_MESSAGE = "Username must be 3-50 chars.";
-    public static final String PASSWORD_MESSAGE = "Password must be 8-30 chars, at least 1 lower case, 1 upper case and 1 special char.";
+    public static final String USERNAME_PATTERN = "^([a-zA-Z0-9_\\-\\.]+)@(([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
+    public static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,30}$";
+    public static final String EMAIL_PATTERN = "^([a-zA-Z0-9_\\-\\.]+)@(([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
+    public static final String USERNAME_MESSAGE = "Email must be of correct format, and between 6-50 chars.";
+    public static final String PASSWORD_MESSAGE = "Password must be 8-30 chars, at least 1 lower case, 1 upper case and 1 numerical.";
     public static final String EMAIL_MESSAGE = "Email must be of correct format, and between 6-50 chars.";
 }
